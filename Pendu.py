2d539d8699de10a9a2d962e73e0e8cf34ef226a8
villamandos <enzzovillamandos@gmail.com>
2025-05-17 15:56:17 -0400

    Fonction pendu

--- Pendu.py
+++ Pendu.py
@@ -22,3 +22,45 @@ def choisirmot():
         mot = mot.replace('ç', 'c')
         mots_sans_accents.append(mot)
     return random.choice(mots_sans_accents)
+
+
+def pendu():
+    print(
+        "Bienvenue au jeu du pendu, l'objectif est de trouver le mot caché derrière les '_' vous avez doit à 6 fautes /n à vous de JOUER ! ")
+
+    solution = choisirmot()
+
+    mot_a_trouver = "_" * len(solution)
+    mot_actualiser = "_" * len(solution)
+    nombre_chance = 6
+    joker = 1
+
+    print("Trouve le mot du pendu (tu as droit à 6 erreurs)")
+    print(mot_a_trouver)
+
+    while nombre_chance != 0:
+        lettre = input("Propose une lettre : ")
+        for i, lettres in enumerate(solution):
+            if lettre == lettres:
+                mot_a_trouver = mot_a_trouver[:i] + lettre + mot_a_trouver[i + 1:]
+
+        if lettre in mot_actualiser:
+            print("La lettre a deja été trouver !")
+        elif mot_a_trouver == mot_actualiser:
+            nombre_chance -= 1
+            print(f"il vous reste {nombre_chance} chances")
+        elif mot_a_trouver == solution:
+            print(f"Tu as gagnée le mot était bien : {solution} ")
+            break
+        else:
+            print("Continue comme ça")
+            print(mot_a_trouver)
+        mot_actualiser = mot_a_trouver
+
+        if nombre_chance == 1 and joker == 1:
+            position = mot_actualiser.find("_")
+            print(f"Teste la lettre {solution[position]} pour voir ;)")
+            joker -= 1
+
+    if nombre_chance == 0:
+        print(f"Tu as perdu, le mot etait : {solution} ")
